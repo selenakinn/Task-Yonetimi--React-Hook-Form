2f98ada2de812ac4674f0a254810c86e0aff3284
Selen Akın <127742376+selenakinn@users.noreply.github.com>
2026-04-28 14:25:23 +0300

    Implement main App component with task management

---
+++ app.jsx
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import './app.css';
+import Task from './components/Task';
+import TaskForm from './components/TaskForm';
+import TaskHookForm from './components/TaskHookForm';
+import PeopleForm from './components/PeopleForm';
+import { initialTasks, initialTeam } from './data';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function App() {
+  const [tasks, setTasks] = useState(initialTasks);
+  const [team, setTeam] = useState(initialTeam);
+
+  function handleTaskSubmit(yeniTask) {
+    setTasks([yeniTask, ...tasks]);
+    toast.success('Yeni görev oluşturuldu.');
+  }
+
+  function handlePeopleSubmit(yeniKisi) {
+    setTeam([...team, yeniKisi]);
+    toast.success('Yeni kişi oluşturuldu.');
+  }
+
+  function handleComplete(id) {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, status: 'yapıldı' } : task
+      )
+    );
+
+    toast.success(`${id} idli görev tamamlandı.`);
+  }
+
+  return (
+    <div className="app">
+      <div className="formColumn">
+        <div className="form-container">
+          <h2>Yeni Task</h2>
+          {/* <TaskForm kisiler={team} submitFn={handleTaskSubmit} /> */}
+          <TaskHookForm kisiler={team} submitFn={handleTaskSubmit} />
+        </div>
+
+        <div className="form-container">
+          <h2>Yeni Kişi</h2>
+          <PeopleForm kisiler={team} submitFn={handlePeopleSubmit} />
+        </div>
+        <ToastContainer />
+      </div>
+      <div className="columns">
+        <div className="column">
+          <h2 className="column-title">Yapılacaklar</h2>
+          <div className="column-list">
+            {tasks
+              .filter((t) => t.status === 'yapılacak')
+              .map((t) => (
+                <Task key={t.id} taskObj={t} onComplete={handleComplete} />
+              ))}
+          </div>
+        </div>
+        <div className="column">
+          <h2 className="column-title">Tamamlananlar</h2>
+          <div className="column-list">
+            {tasks
+              .filter((t) => t.status === 'yapıldı')
+              .map((t) => (
+                <Task key={t.id} taskObj={t} />
+              ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
